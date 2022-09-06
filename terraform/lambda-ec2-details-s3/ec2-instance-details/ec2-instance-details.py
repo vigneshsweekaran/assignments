b1d6696285d504bb4393490d3391c4beb408bcd5
Vignesh Sweekaran <40670015+vigneshsweekaran@users.noreply.github.com>
2022-09-06 12:00:46 +0000

    Updated python script

--- terraform/lambda-ec2-details-s3/ec2-instance-details/ec2-instance-details.py
+++ terraform/lambda-ec2-details-s3/ec2-instance-details/ec2-instance-details.py
@@ -1,8 +1,22 @@
 import boto3
+import csv
 
-def lambda_handler(event, context):
+header = ['Name', 'Type', 'Image-id']
+data = []
+
+def ec2_instance_details(event, context):
     
     ec2_resource = boto3.resource('ec2')
-    instances = [instance.state["Name"] for instance in ec2_resource.instances.all()]
-    
-    print('Running: ', instances.count('running'))
+    for instance in ec2_resource.instances.all():
+        state = instance.state["Name"]
+        type = instance.instance_type
+        image = instance.image
+        print(f"Instance State : {state}, Instance Type : {type}, Instance Image : {image}")
+
+def craete_csv_file(data):
+    with open('ec2-instances.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        # write the header
+        writer.writerow(header)
+        # write multiple rows
+        writer.writerows(data)
